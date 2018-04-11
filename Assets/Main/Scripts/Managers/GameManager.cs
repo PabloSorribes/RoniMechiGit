a259240da4a8acfc3e4fe39d1cf310b9a7c8b3cb
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-04-12 00:57:06 +0200

    added openLevelGongGong & moved hazard events into busses

--- Assets/Main/Scripts/Managers/GameManager.cs
+++ Assets/Main/Scripts/Managers/GameManager.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 /// <summary>
-/// Updates the GameStatus 
+/// Updates the GameStatus
 /// </summary>
-public class GameManager : MonoBehaviour {
-
-	GameStateManager gsManager;
-	NewGameUI gameUI;
+public class GameManager : MonoBehaviour
+{
+	private GameStateManager gsManager;
+	private NewGameUI gameUI;
 
 	//Variables
 	public PlayerHandler playerHandlerPrefab;
+
 	[HideInInspector] public List<PlayerHandler> playerHandlers;
 	public Transform[] spawnPoints;
 	public int alivePlayers;
-	public AudioSource audios;
 
 	public float respawnTime = 1f;
 	public bool useRandomSpawnLocations;
 
-	bool gameStarted;
-	int timesUpdateGameStatusIsCalled = 0;
+	private bool gameStarted;
+	private int timesUpdateGameStatusIsCalled = 0;
 
 	//Action for the GameUI to update the life bars.
 	public static event System.Action<GameManager> OnUpdateGameStatus;
 
 	//Singleton
 	private static GameManager instance;
-	public static GameManager GetInstance() {
+
+	public static GameManager GetInstance()
+	{
 		return instance;
 	}
 
-	private void Awake() {
+	private void Awake()
+	{
 		instance = this;
 	}
 
-	private void Start() {
-
+	private void Start()
+	{
 		gsManager = GameStateManager.GetInstance();
 		gameUI = NewGameUI.GetInstance();
 
 		//TODO: If we are inGame, play the start animation thingy whatevs
-		if (gsManager.currentGameState == GameStateManager.GameState.inGame) {
+		if (gsManager.currentGameState == GameStateManager.GameState.inGame)
+		{
 			//Invoke("StartGame", 1);
 			//Invoke("StartGame", 2);
 			//Invoke("StartGame", 3);
@@ -54,8 +54,8 @@ public class GameManager : MonoBehaviour {
 			StartGame();
 		}
 
-		if (gsManager.currentGameState == GameStateManager.GameState.inMenu) {
-
+		if (gsManager.currentGameState == GameStateManager.GameState.inMenu)
+		{
 			useRandomSpawnLocations = false;
 		}
 
@@ -63,65 +63,69 @@ public class GameManager : MonoBehaviour {
 		//alivePlayers = playerHandlers.Count;
 
 		//Know status for each player
-		foreach (var handler in playerHandlers) {
-			if (handler.playerIndexRobert == 0) {
+		foreach (var handler in playerHandlers)
+		{
+			if (handler.playerIndexRobert == 0)
+			{
 				//but do this in the PlayerHandler / send it's index and player's position to the CameraController
 			}
 		}
 	}
 
-	private void StartGame() {
-
+	private void StartGame()
+	{
 		//Add players depending on how many have joined
 		//foreach (var p_index in gsManager.joinedPlayersInt) {
 		//	AddPlayerHandler(p_index);
 		//}
 
-		foreach (var p_joinedPlayer in gsManager.joinedPlayersList) {
+		foreach (var p_joinedPlayer in gsManager.joinedPlayersList)
+		{
 			AddPlayerHandler(p_joinedPlayer.myIndex);
 		}
-
 
 		UpdateAlivePlayers(true, true);
 
 		//gameUI.StartCounterStuff();
-		//AudioOnStart();
+		AudioOnStart();
 	}
 
-	private void AudioOnStart() {
-		//AUDIO:
-		audios.Play();
+	private void AudioOnStart()
+	{
+		FMODUnity.RuntimeManager.PlayOneShot("event:/openLevelGong");
 	}
 
-	public void AddPlayerHandler(int p_playerIndex) {
+	public void AddPlayerHandler(int p_playerIndex)
+	{
 		var _newPlayerHandler = Instantiate(playerHandlerPrefab, transform);
 		_newPlayerHandler.playerIndexRobert = p_playerIndex;
 		playerHandlers.Add(_newPlayerHandler);
 	}
 
 	//TODO: Fix double spawning when Adding all players → Removing Player1 → Adding Player2 → duplicate Player 2 is added
-	public void AddPlayerHandlerInMenu(int p_playerIndex) {
-
+	public void AddPlayerHandlerInMenu(int p_playerIndex)
+	{
 		var _newPlayerHandler = Instantiate(playerHandlerPrefab, transform);
 		_newPlayerHandler.playerIndexRobert = p_playerIndex;
 
 		//playerHandlers.RemoveAt(p_playerIndex);
 		playerHandlers.Insert(p_playerIndex, _newPlayerHandler);
 
-
 		//TODO: Getting nullrefs here when spawning in an unordered fashion.
-		if (playerHandlers[p_playerIndex].handlerHasSpawned == true) {
-
+		if (playerHandlers[p_playerIndex].handlerHasSpawned == true)
+		{
 			playerHandlers[p_playerIndex].Start();
 		}
 
-		if (playerHandlers[p_playerIndex].handlerHasSpawned == false) {
+		if (playerHandlers[p_playerIndex].handlerHasSpawned == false)
+		{
 			playerHandlers[p_playerIndex].handlerHasSpawned = true;
 		}
 	}
 
 	//EXPERIMENTAL CODE
-	public void RemovePlayerHandler(int p_playerIndex) {
+	public void RemovePlayerHandler(int p_playerIndex)
+	{
 		//var _newPlayerHandler = playerHandlers[p_playerIndex];
 		//_newPlayerHandler.playerIndexRobert = p_playerIndex;
 
@@ -130,31 +134,33 @@ public class GameManager : MonoBehaviour {
 		//playerHandlers.Remove(playerHandlers[p_playerIndex]);
 	}
 
-	void Update() {
+	private void Update()
+	{
 		//print("Amount of playerHandlers: " + playerHandlers.Count);
 		//print("Amount of alive players: " + alivePlayers);
 	}
 
 	/// <summary>
-	/// <paramref name="p_setAmountOnStart"/>: Set to TRUE when using once, eg. in Start(). 
+	/// <paramref name="p_setAmountOnStart"/>: Set to TRUE when using once, eg. in Start().
 	/// Set to FALSE when calling more than once, eg. in UpdateGameStatus().
-	/// 
-	/// <para> <paramref name="p_isPlayerAlive"/>: Takes the "isAlive"-bool from a PlayerHandler. 
+	///
+	/// <para> <paramref name="p_isPlayerAlive"/>: Takes the "isAlive"-bool from a PlayerHandler.
 	/// </para>
 	/// </summary>
 	/// <param name="p_setAmountOnStart"></param>
 	/// <param name="p_isPlayerAlive"></param>
-	public void UpdateAlivePlayers(bool p_setAmountOnStart, bool p_isPlayerAlive) {
-
-		if (p_setAmountOnStart) {
+	public void UpdateAlivePlayers(bool p_setAmountOnStart, bool p_isPlayerAlive)
+	{
+		if (p_setAmountOnStart)
+		{
 			//alivePlayers = gsManager.joinedPlayersInt.Count;
 
 			int _myAmountOfJoinedPlayers = 0;
 
-			foreach (var joinedPlayer in gsManager.joinedPlayersList) {
-
-				if (joinedPlayer.hasBeenSpawned) {
-
+			foreach (var joinedPlayer in gsManager.joinedPlayersList)
+			{
+				if (joinedPlayer.hasBeenSpawned)
+				{
 					_myAmountOfJoinedPlayers++;
 
 					//alivePlayers++;
@@ -166,7 +172,8 @@ public class GameManager : MonoBehaviour {
 			//alivePlayers = gsManager.joinedPlayersList.Count;
 		}
 
-		if (!p_isPlayerAlive) {
+		if (!p_isPlayerAlive)
+		{
 			alivePlayers--;
 		}
 
@@ -177,30 +184,32 @@ public class GameManager : MonoBehaviour {
 	/// Check if we only have one player left, etc.
 	/// </summary>
 	/// <param name="p_playerHandler"></param>
-	public void UpdateGameStatus(PlayerHandler p_playerHandler) {
-
+	public void UpdateGameStatus(PlayerHandler p_playerHandler)
+	{
 		//TODO: Check if we are inGame or not. If yes, add players and shit. Needs refactoring.
-		if (gsManager.currentGameState == GameStateManager.GameState.inGame) {
-
+		if (gsManager.currentGameState == GameStateManager.GameState.inGame)
+		{
 			//Update the players in the CameraController's List of players.
 			Camera.main.GetComponent<CameraController>().UpdatePlayersInList(p_playerHandler);
 
 			UpdateAlivePlayers(false, p_playerHandler.isAlive);
 
 			//Allow the game to be stopped (by eg. postGameUI) only if we FIRST have 2 or more players in the scene, and THEN one of them dies.
-			if (alivePlayers > 1) {
+			if (alivePlayers > 1)
+			{
 				gameStarted = true;
 			}
 
-			//When there's only one player left and the game has already started → 
+			//When there's only one player left and the game has already started →
 			// → show the PostGameUI and send which player is left.
-			if (alivePlayers == 1 && gameStarted) {
-
+			if (alivePlayers == 1 && gameStarted)
+			{
 				gsManager.currentGameState = GameStateManager.GameState.gameOver;
 				//print("Sending call to PostGameInfo");
 				gameUI.PostGameInformation(p_playerHandler);
 			}
-			else if (alivePlayers < 1 && gameStarted) {
+			else if (alivePlayers < 1 && gameStarted)
+			{
 				gsManager.currentGameState = GameStateManager.GameState.gameOver;
 				print("Everybody dieded! D: ");
 			}
