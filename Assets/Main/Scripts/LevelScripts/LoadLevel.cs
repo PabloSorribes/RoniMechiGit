b41c3e7d245d098feb6870daedeb97f45b779039
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-04-12 00:15:56 +0200

    cleaned up level scripts

--- Assets/Main/Scripts/LevelScripts/LoadLevel.cs
+++ Assets/Main/Scripts/LevelScripts/LoadLevel.cs
@@ -1,93 +1,102 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Used when choosing map in menu
-public class LoadLevel : MonoBehaviour {
+/// <summary>
+/// Used when choosing map in menu
+/// </summary>
+public class LoadLevel : MonoBehaviour
+{
 	public MenuManager menuManager;
 	[HideInInspector] public int mapCounter = 0;
 	private int beforeCounter = 0;
-	GameObject player;
+	private GameObject player;
 	public bool playMayanTempel;
 	public bool playSpikeyCavern;
 	public bool playIcicleStuffs;
 
 	private FMODUnity.StudioEventEmitter a_enterLevelSound;
 
-
-	void Start() {
-
-		menuManager = FindObjectOfType<MenuManager>();
-
+	private void Start()
+	{
 		Invoke("GetPlayerOne", 1f);
 
 		a_enterLevelSound = gameObject.AddComponent<FMODUnity.StudioEventEmitter>();
 		a_enterLevelSound.Event = "event:/uiButton_forward";
 	}
 
-	void GetPlayerOne() {
-
+	private void GetPlayerOne()
+	{
 		player = GameManager.GetInstance().playerHandlers[0].playerController.gameObject;
 	}
 
-	private void Update() {
-
+	private void Update()
+	{
 		//reset counter.
 		CounterChecker();
 
-
 		//Check which map is choosen and have the right animation for it.
-		if (mapCounter == 0 && beforeCounter == 2) {
+		if (mapCounter == 0 && beforeCounter == 2)
+		{
 			GetComponent<Animator>().SetTrigger("playIStoSC");
 			beforeCounter = 0;
 		}
-		if (mapCounter == 1 && beforeCounter == 0) {
+		if (mapCounter == 1 && beforeCounter == 0)
+		{
 			GetComponent<Animator>().SetTrigger("playSCtoMT");
 			beforeCounter = 1;
-
 		}
-		if (mapCounter == 2 && beforeCounter == 1) {
+		if (mapCounter == 2 && beforeCounter == 1)
+		{
 			GetComponent<Animator>().SetTrigger("playMTtoIS");
 			beforeCounter = 2;
 		}
-		if (mapCounter == 0 && beforeCounter == 1) {
+		if (mapCounter == 0 && beforeCounter == 1)
+		{
 			GetComponent<Animator>().SetTrigger("playMTtoSC");
 			beforeCounter = 0;
 		}
-		if (mapCounter == 1 && beforeCounter == 2) {
+		if (mapCounter == 1 && beforeCounter == 2)
+		{
 			GetComponent<Animator>().SetTrigger("playIStoMT");
 			beforeCounter = 1;
 		}
-		if (mapCounter == 2 && beforeCounter == 0) {
+		if (mapCounter == 2 && beforeCounter == 0)
+		{
 			GetComponent<Animator>().SetTrigger("playSCtoIS");
 			beforeCounter = 2;
 		}
-
 	}
 
-	//When the player jumps into the image of the level.
-	private void OnTriggerEnter(Collider other) {
-
+	/// <summary>
+	/// When the player jumps into the image of the level.
+	/// </summary>
+	/// <param name="other"></param>
+	private void OnTriggerEnter(Collider other)
+	{
 		a_enterLevelSound.Play();
 
-		if (other.tag == "Player" && mapCounter == 0) {
+		if (other.tag == "Player" && mapCounter == 0)
+		{
 			SceneManager.LoadScene("Spikey Cavern_TestVersion2");
 		}
-		if (other.tag == "Player" && mapCounter == 1) {
-
+		if (other.tag == "Player" && mapCounter == 1)
+		{
 			SceneManager.LoadScene("Mayan Tempel");
 		}
-		if (other.tag == "Player" && mapCounter == 2) {
+		if (other.tag == "Player" && mapCounter == 2)
+		{
 			SceneManager.LoadScene("Icicle stuffs");
 		}
 	}
 
-	private void CounterChecker() {
-		if (mapCounter == 3) {
+	private void CounterChecker()
+	{
+		if (mapCounter == 3)
+		{
 			mapCounter = 0;
 		}
-		if (mapCounter == -1) {
+		if (mapCounter == -1)
+		{
 			mapCounter = 2;
 		}
 	}
--- Assets/Main/Scripts/LevelScripts/TotallyNotAnEasterEgg.cs
+++ Assets/Main/Scripts/LevelScripts/TotallyNotAnEasterEgg.cs
@@ -1,23 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TotallyNotAnEasterEgg : MonoBehaviour
 {
-    public MenuManager menuManager;
-    // Use this for initialization
-    void Start()
-    {
-        menuManager = FindObjectOfType<MenuManager>();
-    }
+	public MenuManager menuManager;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Bullet")
-        {
+	// Use this for initialization
+	private void Start()
+	{
+		menuManager = FindObjectOfType<MenuManager>();
+	}
 
-            SceneManager.LoadScene("Scene_Elton");
-        }
-    }
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Bullet")
+		{
+			SceneManager.LoadScene("Scene_Elton");
+		}
+	}
 }
