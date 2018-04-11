e1e84678cb42ff9c6feec78e262b11bf74d2ab52
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-04-12 01:26:00 +0200

    tabification

--- Assets/Main/Scripts/MenuScripts/ButtonsInMenu.cs
+++ Assets/Main/Scripts/MenuScripts/ButtonsInMenu.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonsInMenu: MonoBehaviour {
-    public Transform playerTargetPosition;
-    public string animationToRun;
-    public string secondAnimationToRun;
+	public Transform playerTargetPosition;
+	public string animationToRun;
+	public string secondAnimationToRun;
 
 	//Creates field in inspector. Drag FmodStudioEventEmitter-component on it. Set event in the component.
 	[SerializeField]
@@ -13,48 +13,48 @@ public class ButtonsInMenu: MonoBehaviour {
 
 	GameStateManager gsManager;
 
-    MenuManager menuManager;
-    GameObject player;
+	MenuManager menuManager;
+	GameObject player;
 
-    public void Start() {
-        menuManager = FindObjectOfType<MenuManager>();
+	public void Start() {
+		menuManager = FindObjectOfType<MenuManager>();
 
-        gsManager = GameStateManager.GetInstance();
+		gsManager = GameStateManager.GetInstance();
 
-        Invoke("GetPlayerOne", 1f);
+		Invoke("GetPlayerOne", 1f);
 
 		FmodComponent_forwardBackward = GetComponent<FMODUnity.StudioEventEmitter>();
-    }
+	}
 
-    //TODO: Improve the getting of the player
-    void GetPlayerOne() {
-        player = GameManager.GetInstance().playerHandlers[0].playerController.gameObject;
-    }
+	//TODO: Improve the getting of the player
+	void GetPlayerOne() {
+		player = GameManager.GetInstance().playerHandlers[0].playerController.gameObject;
+	}
 
-    private void OnTriggerEnter(Collider other) {
+	private void OnTriggerEnter(Collider other) {
 
-        //Animate camera, teleport player and update the Menu Status
-        if (other.tag == "Bullet" || other.tag == "Player") {
+		//Animate camera, teleport player and update the Menu Status
+		if (other.tag == "Bullet" || other.tag == "Player") {
 
-            menuManager.anim.SetTrigger(animationToRun);
-            player.transform.position = playerTargetPosition.position;
+			menuManager.anim.SetTrigger(animationToRun);
+			player.transform.position = playerTargetPosition.position;
 
-            UpdateMenuStatus(animationToRun);
+			UpdateMenuStatus(animationToRun);
 
 			FmodComponent_forwardBackward.Play();
 		}
 
-    }
+	}
 
-    //TODO: Add statements for each animation that moves the camera to a different state.
-    void UpdateMenuStatus(string p_animationToRun) {
-        if (p_animationToRun == "BackPSelect") {
-            gsManager.currentMainMenuState = GameStateManager.MenuState.start;
-        }
+	//TODO: Add statements for each animation that moves the camera to a different state.
+	void UpdateMenuStatus(string p_animationToRun) {
+		if (p_animationToRun == "BackPSelect") {
+			gsManager.currentMainMenuState = GameStateManager.MenuState.start;
+		}
 
-        //Players can join the game
-        else if (p_animationToRun == "toPlayerSelection" || p_animationToRun == "BackLSelect") {
-            gsManager.currentMainMenuState = GameStateManager.MenuState.playerSelection;
-        }
-    }
+		//Players can join the game
+		else if (p_animationToRun == "toPlayerSelection" || p_animationToRun == "BackLSelect") {
+			gsManager.currentMainMenuState = GameStateManager.MenuState.playerSelection;
+		}
+	}
 }
