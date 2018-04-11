1dd1c0ecae6b2b15ec640ed218b1466441243470
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-04-12 00:10:03 +0200

    cleaned up BearTrap & dissapearingPlatform

--- Assets/Main/Scripts/Hazards/BearTrapScript.cs
+++ Assets/Main/Scripts/Hazards/BearTrapScript.cs
@@ -1,69 +1,64 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BearTrapScript : MonoBehaviour {
-    private GameManager gameManager;
+public class BearTrapScript : MonoBehaviour
+{
+	public float deathAnimationTime = 0.5f;
+	private bool closed = false;
 
-    public float deathAnimationTime = 0.5f;
+	public GameObject particleSpark;
+	public GameObject leftSideTrap;
+	public GameObject rightSideTrap;
 
-    private bool closed = false;
+	// Use this for initialization
+	private void Start()
+	{
+		leftSideTrap.GetComponent<Animator>();
+		rightSideTrap.GetComponent<Animator>();
+	}
 
-    public GameObject particleSpark;
-    public GameObject leftSideTrap;
-    public GameObject rightSideTrap;
-    // Use this for initialization
-    void Start () {
-        gameManager = FindObjectOfType<GameManager>();
-        leftSideTrap.GetComponent<Animator>();
-        rightSideTrap.GetComponent<Animator>();
-    }
+	private void OnTriggerStay(Collider p_other)
+	{
+		if (p_other.tag == "Player" && closed == false)
+		{
+			print(p_other.GetComponent<NewPlayerController>().canMove);
+			p_other.GetComponent<NewPlayerController>().canMove = false;
+			print(p_other.GetComponent<NewPlayerController>().canMove);
 
-    private void OnTriggerStay (Collider p_other)
-    {
-        if (p_other.tag == "Player" && closed == false)
-        {
-            print(p_other.GetComponent<NewPlayerController>().canMove);
-            p_other.GetComponent<NewPlayerController>().canMove = false;
-            print(p_other.GetComponent<NewPlayerController>().canMove);
+			closed = true;
+			leftSideTrap.GetComponent<Animator>().SetTrigger("Close");
+			rightSideTrap.GetComponent<Animator>().SetTrigger("Close");
+			GameObject newEffect = Instantiate(particleSpark, p_other.transform.position, this.transform.rotation);
+			Destroy(newEffect, 3f);
 
-            closed = true;
-            leftSideTrap.GetComponent<Animator>().SetTrigger("Close");
-            rightSideTrap.GetComponent<Animator>().SetTrigger("Close");
-            GameObject newEffect = Instantiate(particleSpark, p_other.transform.position, this.transform.rotation);
-            Destroy(newEffect, 3f);
+			StartCoroutine(KillOnTime(p_other));
 
-            //Invoke("KillOnTime",0.5f);
-            StartCoroutine(KillOnTime(p_other));
+			leftSideTrap.GetComponent<Animator>().SetTrigger("Open");
+			rightSideTrap.GetComponent<Animator>().SetTrigger("Open");
+			Invoke("WaitforClosed", 2.6f);
+		}
 
-            leftSideTrap.GetComponent<Animator>().SetTrigger("Open");
-            rightSideTrap.GetComponent<Animator>().SetTrigger("Open");
-            Invoke("WaitforClosed", 2.6f);
-        }
+		if ((p_other.tag == "Bullet" || p_other.tag == "Icicle") && closed == false)
+		{
+			closed = true;
+			leftSideTrap.GetComponent<Animator>().SetTrigger("Close");
+			rightSideTrap.GetComponent<Animator>().SetTrigger("Close");
 
+			leftSideTrap.GetComponent<Animator>().SetTrigger("Open");
+			rightSideTrap.GetComponent<Animator>().SetTrigger("Open");
+			Invoke("WaitforClosed", 2.6f);
+		}
+	}
 
-        if((p_other.tag == "Bullet" || p_other.tag == "Icicle") && closed == false)
-        {
-            closed = true;
-            leftSideTrap.GetComponent<Animator>().SetTrigger("Close");
-            rightSideTrap.GetComponent<Animator>().SetTrigger("Close");
+	private void WaitforClosed()
+	{
+		closed = false;
+	}
 
-            leftSideTrap.GetComponent<Animator>().SetTrigger("Open");
-            rightSideTrap.GetComponent<Animator>().SetTrigger("Open");
-            Invoke("WaitforClosed", 2.6f);
-        }
-    }
+	private IEnumerator KillOnTime(Collider p_player)
+	{
+		yield return new WaitForSeconds(deathAnimationTime);
 
-    void WaitforClosed()
-    {
-        closed = false;
-    }
-
-    IEnumerator KillOnTime(Collider p_player)
-    {
-        yield return new WaitForSeconds(deathAnimationTime);
-
-        p_player.gameObject.GetComponent<NewPlayerController>().playerHandler.KillPlayer();
-
-    }
+		p_player.gameObject.GetComponent<NewPlayerController>().playerHandler.KillPlayer();
+	}
 }
--- Assets/Main/Scripts/Hazards/disappearingPlatform.cs
+++ Assets/Main/Scripts/Hazards/disappearingPlatform.cs
@@ -1,51 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class disappearingPlatform : MonoBehaviour {
+public class disappearingPlatform : MonoBehaviour
+{
+	public int time;
+	private BoxCollider platformCollider;
+	private BoxCollider childCollider;
+	private MeshRenderer platform;
+	private Transform childTransform;
 
-    public int time;
-    BoxCollider platformCollider;
-    BoxCollider childCollider;
-    MeshRenderer platform;
-    private Transform childTransform;
+	// Use this for initialization
+	private void Start()
+	{
+		platformCollider = gameObject.AddComponent<BoxCollider>();
+		platform = GetComponentInChildren<MeshRenderer>();
+		childCollider = GetComponentInChildren<BoxCollider>();
 
+		platformCollider.enabled = false;
+		platform.enabled = false;
+		childCollider.enabled = false;
 
+		childTransform = transform.GetChild(0);
+		platformCollider.size = new Vector3(childTransform.transform.localScale.x, childTransform.transform.localScale.y, childTransform.transform.localScale.z);
+	}
 
-    // Use this for initialization
-    void Start ()
-    {
-        platformCollider = gameObject.AddComponent<BoxCollider>();
-        platform = GetComponentInChildren<MeshRenderer>();
-        childCollider = GetComponentInChildren<BoxCollider>();
-
-        platformCollider.enabled = false;
-        platform.enabled = false;
-        childCollider.enabled = false;
-
-        childTransform = transform.GetChild(0);
-        platformCollider.size = new Vector3(childTransform.transform.localScale.x, childTransform.transform.localScale.y, childTransform.transform.localScale.z);
-
-    }
-	
 	// Update is called once per frame
-	void Update ()
-    {
-
+	private void Update()
+	{
 		if (time > 800 && time < 1500)
-        {
-            platformCollider.enabled = true;
-            platform.enabled = true;
-           
-        }
-        
-        if (time > 2000)
-        {
-            platformCollider.enabled = false;
-            platform.enabled = false;
-            time = 0;
-        }
+		{
+			platformCollider.enabled = true;
+			platform.enabled = true;
+		}
 
-        time++;
+		if (time > 2000)
+		{
+			platformCollider.enabled = false;
+			platform.enabled = false;
+			time = 0;
+		}
+
+		time++;
 	}
 }
