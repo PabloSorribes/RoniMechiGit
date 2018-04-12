58718bf4fa82cbb73f080b30d4250e3aa8aa8ac9
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2018-04-12 03:32:27 +0200

    tabs

--- Assets/Main/Scripts/Player/PlayerUI.cs
+++ Assets/Main/Scripts/Player/PlayerUI.cs
@@ -4,50 +4,50 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour {
-    
-    //To get currect amount of health
-    private NewPlayerController myPlayer;
+	
+	//To get currect amount of health
+	private NewPlayerController myPlayer;
 	private Canvas gameCanvas;
 
-    [Header("--- Ammo Slider Properties ---")]
-    public GameObject healthBarSliderPrefab;
+	[Header("--- Ammo Slider Properties ---")]
+	public GameObject healthBarSliderPrefab;
 
-    public Vector3 offset;
-    public Vector3 offsetHealthBar;
+	public Vector3 offset;
+	public Vector3 offsetHealthBar;
 
-    private GameObject healthBarSliderHolder;
-    private Slider healthBarSliderRef;
+	private GameObject healthBarSliderHolder;
+	private Slider healthBarSliderRef;
 
-    void Start () {
-        myPlayer = GetComponent<NewPlayerController>();
+	void Start () {
+		myPlayer = GetComponent<NewPlayerController>();
 		gameCanvas = FindObjectOfType<Canvas>();
 
-        ActivateUI();
-    }
+		ActivateUI();
+	}
 
-    private void ActivateUI() {
-        //- Instantiate the Slider-prefabs and sets it at the position of the canvas.
-        healthBarSliderHolder = Instantiate(healthBarSliderPrefab, FindObjectOfType<Canvas>().transform);
-        healthBarSliderHolder.GetComponentInChildren<Text>().text = "unknown";
+	private void ActivateUI() {
+		//- Instantiate the Slider-prefabs and sets it at the position of the canvas.
+		healthBarSliderHolder = Instantiate(healthBarSliderPrefab, FindObjectOfType<Canvas>().transform);
+		healthBarSliderHolder.GetComponentInChildren<Text>().text = "unknown";
 
 		//- Set the ammoSliderRef to be able to change its values in Update.
 		healthBarSliderRef = healthBarSliderHolder.GetComponent<Slider>();
 
 		//- Text above player's head.
-        if (myPlayer.currentPlayer == "_P1")
-            healthBarSliderHolder.GetComponentInChildren<Text>().text = "P1";
+		if (myPlayer.currentPlayer == "_P1")
+			healthBarSliderHolder.GetComponentInChildren<Text>().text = "P1";
 
-        if (myPlayer.currentPlayer == "_P2")
-            healthBarSliderHolder.GetComponentInChildren<Text>().text = "P2";
+		if (myPlayer.currentPlayer == "_P2")
+			healthBarSliderHolder.GetComponentInChildren<Text>().text = "P2";
 
-        if (myPlayer.currentPlayer == "_P3")
-            healthBarSliderHolder.GetComponentInChildren<Text>().text = "P3";
+		if (myPlayer.currentPlayer == "_P3")
+			healthBarSliderHolder.GetComponentInChildren<Text>().text = "P3";
 
-        if (myPlayer.currentPlayer == "_P4")
-            healthBarSliderHolder.GetComponentInChildren<Text>().text = "P4";
-    }
+		if (myPlayer.currentPlayer == "_P4")
+			healthBarSliderHolder.GetComponentInChildren<Text>().text = "P4";
+	}
 
-    void Update () {
+	void Update () {
 		ChooseMyUIPosition();
 
 		//TODO: GUI and alignElement.cs make no sense.
@@ -55,9 +55,9 @@ public class PlayerUI : MonoBehaviour {
 		//- Move the sliders with the player.
 		healthBarSliderHolder.transform.position = Camera.main.WorldToScreenPoint(transform.position + offsetHealthBar);
 
-        // Update the value of the sliders
-        healthBarSliderRef.value = myPlayer.playerHandler.lifeLeft;
-    }
+		// Update the value of the sliders
+		healthBarSliderRef.value = myPlayer.playerHandler.lifeLeft;
+	}
 
 	private void ChooseMyUIPosition() {
 
@@ -76,7 +76,7 @@ public class PlayerUI : MonoBehaviour {
 
 			healthBarSliderHolder.transform.position = Camera.main.WorldToScreenPoint(_uiPosition);
 
-        }
+		}
 		if (_myIndex == 1) {
 			//Up-Right
 		}
@@ -93,6 +93,6 @@ public class PlayerUI : MonoBehaviour {
 	}
 
 	public void DestroyUI() {
-        Destroy(healthBarSliderHolder);
-    }
+		Destroy(healthBarSliderHolder);
+	}
 }
